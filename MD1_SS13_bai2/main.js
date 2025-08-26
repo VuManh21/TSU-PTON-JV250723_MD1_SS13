6623f1f23d8ab2ce9aa8e10845cde3e2bb227408
VuManh21 <87159958+VuManh21@users.noreply.github.com>
2025-08-27 00:12:47 +0900

    TSU-PTON-JV250723_MD1_SS13_BAI02

---
+++ MD1_SS13_bai2/main.js
@@ -0,0 +1,7 @@
+let numBer = [1, 3, 5, 6 ,7];
+let choose = Number(prompt("hay chon so bat ki tu 1 den 10"))
+if(numBer.includes(choose)){
+    alert("Bingo");
+}else{
+    alert("chuc ban may man lan sau");
+}
